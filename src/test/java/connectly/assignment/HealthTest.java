d325ff103addfa149d8fae4c375bdd7e4d520ff7
sjk5766 <wjdrnr6662@gmail.com>
2023-02-01 14:00:37 +0900

    test: health check 테스트 케이스 추가

---
+++ src/test/java/connectly/assignment/HealthTest.java
@@ -0,0 +1,33 @@
+package connectly.assignment;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HealthTest extends AcceptanceTest {
+	@Test
+	@DisplayName("헬스체크를 통해 Application, DB 구동 상태를 확인할 수 있다.")
+	void health() {
+		// when
+		JsonPath response = RestAssured
+				.given().log().all()
+				.when().get("/actuator/health")
+				.then().log().all()
+				.extract()
+				.jsonPath();
+
+		// then
+		String applicationStatus = response.getString("status");
+		String dbStatus = response.getString("components.db.status");
+
+		assertThat(applicationStatus).isEqualTo("UP");
+		assertThat(dbStatus).isEqualTo("UP");
+	}
+}
